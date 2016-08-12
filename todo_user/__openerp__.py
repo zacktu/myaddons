0fb2534ca6b0532e1a82379ac3967c7ff408c36b
bob <cannon@sc.edu>
2016-08-12 10:31:41 -0400

    My incremental addition of files by following the textbook resulted in an error wouldn't let the server start and that I couldn't find.  I've copied all of the files from the book's code into this directory.  Let's see whether this works.

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -1,6 +1,8 @@
-{   
+{
     'name': 'Multiuser To-Do',
-    'description': 'Extend the To-Do app to multiuser.',
+    'description': 'Extend To-Do Tasks for multiuser',
     'author': 'Bob Cannon',
     'depends': ['todo_app'],
+    'data': ['todo_view.xml', 'security/todo_access_rules.xml'],
+    'demo': ['todo.task.csv', 'todo_data.xml'],
 }
--- todo_user/todo_task.py
+++ todo_user/todo_task.py
@@ -3,20 +3,12 @@ from openerp import models, fields, api
 
 
 class TodoTask(models.Model):
-    _name = 'todo_task'
-    # _inherit = ['todo.task', 'mail.thread']
-    _inherit = 'todo.task'
+    _name = 'todo.task'
+    _inherit = ['todo.task', 'mail.thread']
 
     user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
     name = fields.Char(help="What needs to be done?")
-
-    @api.one
-    def do_toggle_done(self):
-        if self.user_id != self.env.user:
-            raise Exception('Only the responsible user can do this!')
-        else:
-            return super(TodoTask, self).do_toggle_done()
 
     @api.multi
     def do_clear_done(self):
@@ -27,4 +19,9 @@ class TodoTask(models.Model):
         done_recs.write({'active': False})
         return True
 
-
+    @api.one
+    def do_toggle_done(self):
+        if self.user_id != self.env.user:
+            raise Exception('Only the responsible can do this!')
+        else:
+            return super(TodoTask, self).do_toggle_done()
