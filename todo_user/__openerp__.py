ef9235230d2bd903938c268e9e5aff1ca8185396
bob <cannon@sc.edu>
2016-08-12 11:34:32 -0400

    Effort to use all the code for chapters 3 & 4 didn't work.  I cut back to code through p. 45, and everything appears to work.  I've created two new users, Fred and Ethel and given them passwords and also give the demo user a password that I know.  I let Fred and Ethel each assign tasks and also delete a task.  Then Fred and Ethel each designated a task as done.  I then came back and had Fred clear all done tasks.  His done task was cleared, and Ethel's done task was not cleared.  All appears to be working.  Also, the administrator can see all of the tasks.

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -3,6 +3,7 @@
     'description': 'Extend To-Do Tasks for multiuser',
     'author': 'Bob Cannon',
     'depends': ['todo_app'],
-    'data': ['todo_view.xml', 'security/todo_access_rules.xml'],
-    'demo': ['todo.task.csv', 'todo_data.xml'],
+    'data': ['todo_view.xml'],
+    # 'data': ['todo_view.xml', 'security/todo_access_rules.xml'],
+    #'demo': ['todo.task.csv', 'todo_data.xml'],
 }
--- todo_user/todo_task.py
+++ todo_user/todo_task.py
@@ -3,8 +3,9 @@ from openerp import models, fields, api
 
 
 class TodoTask(models.Model):
-    _name = 'todo.task'
-    _inherit = ['todo.task', 'mail.thread']
+    #_name = 'todo.task'
+    #_inherit = ['todo.task', 'mail.thread']
+    _inherit = ['todo.task']
 
     user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
@@ -14,7 +15,7 @@ class TodoTask(models.Model):
     def do_clear_done(self):
         domain = [('is_done', '=', True),
                   '|', ('user_id', '=', self.env.uid),
-                  ('user_id', '=', False)]
+                       ('user_id', '=', False)]
         done_recs = self.search(domain)
         done_recs.write({'active': False})
         return True
