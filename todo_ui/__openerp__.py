ab95acb9bba7dfabd62e76036ab237e1bb5542be
bob <cannon@sc.edu>
2016-08-19 11:04:52 -0400

    Baseline for Chapter 5.  Some lines will be deleted before initial use.

---
+++ todo_ui/__openerp__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'User interface improvements to the To-Do app',
+    'description': 'User friendly features.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_user'],
+    'data': ['todo_view.xml'],
+}
