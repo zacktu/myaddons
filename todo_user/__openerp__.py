d4a88f6acc405b48cc5b3f8a94566a6517c5f606
bob <cannon@sc.edu>
2016-08-19 10:25:03 -0400

    Finished Chapter 4.

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -4,6 +4,6 @@
     'author': 'Bob Cannon',
     'depends': ['todo_app'],
     'data': ['todo_view.xml', 'security/todo_access_rules.xml'],
-    'demo': ['todo.task.csv',],
-    #'demo': ['todo.task.csv', 'todo_data.xml'],
+    #'demo': ['todo.task.csv',],
+    'demo': ['todo.task.csv', 'todo_data.xml'],
 }
